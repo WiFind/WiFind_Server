3c0f2352a55b6ab50ff01b0426dfded48f7e15a8
muyangwang <muyang@umich.edu>
2015-11-20 18:31:44 -0500

    add location page

--- nursecall/views.py
+++ nursecall/views.py
@@ -41,7 +41,7 @@ def getList(request):
         newLine += '<p><a href ="' +location_url +'" style="font-size: 24px !important;">Get Location</a><br/>'
         if help==True:
             newLine += '<div style="font-size: 20px !important; color:red">I NEED HELP!</div>'
-        newLine += '<div>patient descriptions here</div></p>'
+        newLine += '<div>patient descriptions here: addr  ' + mac_addr +'</div></p>'
         newLine += '</li>'
         contents += newLine
     style = '<style>li:hover{background: #eee;}\ndiv{margin: 20px}\nh3{font: bold 20px/1.5 Helvetica, Verdana, sans-serif;}\nli p{font: 200 16px/1.5 Georgia, Times New Roman, serif;}\nli img{float:left;margin:0 15px 0 0;width:250px;height:200px;}</style>'
