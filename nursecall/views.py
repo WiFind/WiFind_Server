d909bd58610427aa95e1206051fb33c6d4e2ffbf
muyangwang <muyang@umich.edu>
2015-11-24 20:15:41 -0500

    add refresh

--- nursecall/views.py
+++ nursecall/views.py
@@ -33,18 +33,32 @@ def getList(request):
         mac_addr = eachDevice.mac_addr
         owner = eachDevice.owner.name
         map_url = eachDevice.map_url
-        help = eachDevice.help_req
-        # style taken from http://designshack.net/articles/css/5-simple-and-practical-css-list-styles-you-can-copy-and-paste/
-        newLine = '<li style = "padding:10px; overflow:auto;"><img src = "http://40.media.tumblr.com/9441a09bddeafcfe4ea3a5a826195673/tumblr_nhgjoiad8H1s420s7o1_500.png" />'
-        newLine += '<h3>' + owner + '</h3>'
-        location_url = 'http://' + get_current_site(request).domain + '/loc/' + mac_addr + '.png'
-        newLine += '<p><a href ="' +location_url +'" style="font-size: 24px !important;">Get Location</a><br/>'
-        if help==True:
+        if eachDevice.help_req == True:
+            # style taken from http://designshack.net/articles/css/5-simple-and-practical-css-list-styles-you-can-copy-and-paste/
+            location_url = 'http://' + get_current_site(request).domain + '/loc/' + mac_addr + '.png'
+            newLine = '<li style = "padding:10px; overflow:auto;"><img src = "' + location_url +'" />'
+            #newLine = '<li style = "padding:10px; overflow:auto;"><img src = "" />'
+            newLine += '<h3>' + owner + '</h3>'
+            newLine += '<p><a href ="' +location_url +'" style="font-size: 24px !important;">Get Location</a><br/>'
             newLine += '<div style="font-size: 20px !important; color:red">I NEED HELP!</div>'
-        newLine += '<div>patient descriptions here: addr  ' + mac_addr +'</div></p>'
-        newLine += '</li>'
-        contents += newLine
+            newLine += '<div>patient descriptions here: addr  ' + mac_addr +'</div></p>'
+            newLine += '</li>'
+            contents += newLine
+    d = Device.objects.all()
+    for eachDevice in d:
+        mac_addr = eachDevice.mac_addr
+        owner = eachDevice.owner.name
+        map_url = eachDevice.map_url
+        if eachDevice.help_req == 0:
+            # style taken from http://designshack.net/articles/css/5-simple-and-practical-css-list-styles-you-can-copy-and-paste/
+            location_url = 'http://' + get_current_site(request).domain + '/loc/' + mac_addr + '.png'
+            newLine = '<li style = "padding:10px; overflow:auto;"><img src = "' + location_url + '" />'
+            newLine += '<h3>' + owner + '</h3>'
+            newLine += '<p><a href ="' +location_url +'" style="font-size: 24px !important;">Get Location</a><br/>'
+            newLine += '<div>patient descriptions here: addr  ' + mac_addr +'</div></p>'
+            newLine += '</li>'
+            contents += newLine
     style = '<style>li:hover{background: #eee;}\ndiv{margin: 20px}\nh3{font: bold 20px/1.5 Helvetica, Verdana, sans-serif;}\nli p{font: 200 16px/1.5 Georgia, Times New Roman, serif;}\nli img{float:left;margin:0 15px 0 0;width:250px;height:200px;}</style>'
-    html = '<html><head>'+ style +'</head><body><div style = "margin:20px;"><ul style = "list-style-type:none width:500px;">' + contents + '</ul></div></body></html>'
+    html = '<html><meta http-equiv="Refresh" content="120"><head>'+ style +'</head><body><div style = "margin:20px;"><ul style = "list-style-type:none width:500px;">' + contents + '</ul></div></body></html>'
     return HttpResponse(html)
     pass
